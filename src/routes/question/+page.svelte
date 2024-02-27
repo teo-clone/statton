aa7fb935e5b088bb1799da24d18c3197ef64da4a
teo-clone <theodore.tsivranidis@gmail.com>
2024-02-27 14:30:56 -0500

    using input hidden instead of appearance none

--- src/routes/question/+page.svelte
+++ src/routes/question/+page.svelte
@@ -16,13 +16,7 @@
 	<div class="flex flex-col gap-2 text-xl">
 		{#each data.question.answers as option, i}
 			<label class="flex w-fit cursor-pointer">
-				<input
-					type="radio"
-					name="selectedAnswer"
-					value={option.id}
-					required
-					class="appearance-none"
-				/>
+				<input type="radio" name="selectedAnswer" value={option.id} required hidden />
 				<div
 					class="hover:bg-black hover:text-white label-checked:bg-black label-checked:text-white"
 				>
