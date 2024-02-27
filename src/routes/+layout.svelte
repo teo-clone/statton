8d895ea997428efe55ec72ffff95461d064cdfc6
teo-clone <theodore.tsivranidis@gmail.com>
2024-02-27 18:17:41 -0500

    added a fun rotating cube to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 </script>
 
 <div id="header" class="border-b">
-	<div class="w-fit border-r px-10 py-5 text-lg font-bold">STATTON</div>
+	<div class="w-fit border-r px-10 flex gap-5 items-center">
+		<img class="w-[30px]" src={'/cube.svg'} alt="3d rotating cube" />
+		<div class="w-fit py-5 text-lg font-bold">STATTON</div>
+	</div>
 </div>
 
 <div class="max-w-[600px] px-10 py-5 flex flex-col gap-5">
